1f3deb69cf5a15d10916f52ac876b3bbff51a760
Mesach Venegas <mesach.venegas@hotmail.com>
2022-12-04 16:38:43 -0800

    add: add fuction cart counter

--- Js/main.js
+++ Js/main.js
@@ -1,12 +1,16 @@
-const changeMode = () =>{
+import { items } from './_prodcuts.mjs';
+
+const cartItems = [];
+
+const changeMode = () => {
     const mode = document.getElementById('mode');
     const webContent = document.getElementById('body');
-    mode.addEventListener('click', e =>{
+    mode.addEventListener('click', e => {
         webContent.classList.toggle('dark')
     })
 }
 
-const menuToggle = () =>{
+const menuToggle = () => {
     const btnMenu = document.getElementById('menu');
     const menu = document.getElementById('nav-menu');
     const menuLink = document.querySelectorAll('.menu__link');
@@ -14,16 +18,16 @@ const menuToggle = () =>{
     const stripe1 = document.getElementById('stripe-1')
     const stripe2 = document.getElementById('stripe-2')
     const stripe3 = document.getElementById('stripe-3')
-    btnMenu.addEventListener('click', e =>{
+    btnMenu.addEventListener('click', e => {
         menu.classList.toggle('navbar__menu--open')
         stripe1.classList.toggle('stripe1__menu--open')
         stripe2.classList.toggle('stripe2__menu--open')
         stripe3.classList.toggle('stripe3__menu--open')
     })
 
-    menu.addEventListener('click', e =>{
-        if(e.target.tagName == "A"){
-            menuLink.forEach(link =>{
+    menu.addEventListener('click', e => {
+        if (e.target.tagName == "A") {
+            menuLink.forEach(link => {
                 link.classList.remove('active')
             })
             e.target.classList.add('active')
@@ -37,48 +41,76 @@ const menuToggle = () =>{
 
 // load products of inventory
 const loadProducts = (items) => {
-    items.forEach(item => {
+    items.forEach(({ id, name, price, image, category, quantity }) => {
         const product = document.createElement("div")
-        product.classList.add("product-item")
+        product.classList.add("item__card")
+        product.setAttribute('id',`item-${id}`)
         product.innerHTML = `
+        <div class="item__img">
+                    <img class="item__card__img" src="${image}" alt="${name}">
+                </div>
+                <div class="item__description">
+                    <div class="item__wherehouse">
+                        <span class="item__price item__price--products">$${price.toFixed(2)}</span>
+                        <hr class="item__separator">
+                        <span class="item__stock">Stock: ${quantity}</span>
+                    </div>
+                    <h3 class="item__category">${category}</h3>
+                </div>
+                <button class="btn item__btn--add" id="${id}">+</button>
         `;
-        document.querySelector(".products-list").appendChild(product)
+        document.querySelector(".products__container").appendChild(product)
     });
 };
 
-const cartToggle = () =>{
+const cartToggle = () => {
     const cartOpen = document.getElementById('cart');
     const cartclose = document.getElementById('cart-close');
     const cart = document.getElementById('cart-body');
     const overlay = document.getElementById('overlay-cart')
     const cartItems = document.getElementById('cart-items')
-    cartOpen.addEventListener('click', e =>{
+    cartOpen.addEventListener('click', e => {
         cart.classList.add('cart--open')
         cartItems.classList.add('cart--open')
         overlay.classList.add('cart__overlay--open')
     })
 
-    cartclose.addEventListener('click', e =>{
+    cartclose.addEventListener('click', e => {
         cart.classList.remove('cart--open')
         cartItems.classList.remove('cart--open')
         overlay.classList.remove('cart__overlay--open')
     })
 }
 
-const navScroll = () =>{
+const navScroll = () => {
     const navbar = document.querySelector('.navbar')
-    window.onscroll = () =>{
-        if(window.scrollY > 0){
+    window.onscroll = () => {
+        if (window.scrollY > 0) {
             navbar.classList.add('navbar--move')
-        }else if(window.scrollY == 0){
+        } else if (window.scrollY == 0) {
             navbar.classList.remove('navbar--move')
         }
     }
 }
 
-document.addEventListener('DOMContentLoaded', () =>{
+const addProduct = () =>{
+    const productContainer = document.querySelector('.products__container')
+    const counter = document.getElementById('cart-counter')
+    productContainer.addEventListener('click',e =>{
+        if(e.target.tagName == "BUTTON"){
+            let selected = items.filter(item => item.id == e.target.attributes.id.value)
+            cartItems.push(selected)
+            counter.innerHTML = `${cartItems.length}`
+            console.log(cartItems);
+        }
+    })
+}
+
+document.addEventListener('DOMContentLoaded', () => {
     changeMode()
     menuToggle()
     cartToggle()
     navScroll()
+    loadProducts(items)
+    addProduct()
 })
